fd044d2bb738512faa90bfdc08453fb3885fc0fb
Andres Galindo <me@andresgalindo.us>
2013-08-29 20:01:02 -0400

    Showing name in exception for clarity's sake.

--- src/HL7/Segment.php
+++ src/HL7/Segment.php
@@ -60,7 +60,7 @@ class Segment {
         // Is the name 3 upper case characters?
         //
         if ((!$name) || (strlen($name) != 3) || (strtoupper($name) != $name)) {
-            throw new InvalidArgumentException("Name should be 3 characters, uppercase");
+            throw new InvalidArgumentException("Name should be 3 characters, uppercase. Name provided: {$name}");
         }
 
         $this->_fields = array();
